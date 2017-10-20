14b8bb32ea1829014f8e34d91bbc91346191256a
Mark Poepping <mark.poepping@mmpro.de>
2017-10-20 15:48:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# File Extension List
+This little helper creates a list of file extensions including their "real" name and mimetype.
+
+## Installation
+TBC
+
+## License
+TBC
---
+++ createFile.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+const index = _.last(__filename.split('/'))
+const sourceFiles = _.pull(fs.readdirSync('./sources'), index)
+const fileToWrite = './index.js'
+
+let sources = []
+
+_.forEach(sourceFiles, function(file) {
+  let source = JSON.parse(fs.readFileSync('./sources/' + file).toString())
+  sources    = _.concat(sources, source)
+})
+
+
+// write to file
+let data = 'module.exports = ' + JSON.stringify(sources, null, 2)
+fs.writeFile(fileToWrite, data, function(err) {
+  if (err) throw err
+})
